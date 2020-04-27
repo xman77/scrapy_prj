79631e94377a1466ac3d1dfaee434a8260f60cf4
xman77 <xman77@gmail.com>
2020-04-28 01:49:45 +0800

    modify to select the element of link

--- ScrapyAP.py
+++ ScrapyAP.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 
 response = requests.get(f"https://travel.ettoday.net/category/%E6%A1%83%E5%9C%92/")
 soup = BeautifulSoup(response.text,"html.parser")
-result = soup.find("h3", itemprop="headline")
-print(result.select_one("a"))
-
-# print(soup.prettify())
+titles = soup.find_all("h3", itemprop="headline")
+for title in titles:
+    print(title.select_one("a"))
